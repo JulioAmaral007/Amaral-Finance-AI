cbbb9a126a05d9c842d96f82850931b540fe5e19
Júlio Cézar <jczrpereira77@gmail.com>
2024-11-09 10:07:18 -0300

    feat: add transaction type badge

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,3 +1,22 @@
-export default function TransactionsPage() {
-  return <h1>transactions page</h1>;
+import { Button } from "@/_components/ui/button";
+import { DataTable } from "@/_components/ui/data-table";
+import { db } from "@/_lib/prisma";
+import { ArrowDownUpIcon } from "lucide-react";
+import { transactionColumns } from "./_columns";
+
+export default async function TransactionsPage() {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* TÍTULO E BOTÃO */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ export default {
   theme: {
     extend: {
       colors: {
+        danger: "#F6352E",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         card: {
