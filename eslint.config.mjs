43cdea9b81bff5c33c5556e6a1d4e737cf628cd2
Júlio Cézar <jczrpereira77@gmail.com>
2024-12-25 22:15:54 -0300

    remove: unused files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,20 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends(
-    "@rocketseat/eslint-config/next",
-    "next/core-web-vitals",
-    "next/typescript",
+    '@rocketseat/eslint-config/next',
+    'next/core-web-vitals',
+    'next/typescript',
   ),
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
