589602391f8088f906c7e046894bfd8d6f2f7398
Júlio Cézar <jczrpereira77@gmail.com>
2025-05-23 18:04:20 -0300

    refactor(layout): replace Geist with Mulish font, reformat code, and update Biome settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Mulish } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const mulish = Mulish({
+  subsets: ['latin-ext'],
 })
 
 export const metadata: Metadata = {
@@ -24,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${mulish.className} dark antialiased`}>{children}</body>
     </html>
   )
 }
