474293d0de47e74403be8efeea95371625ecc4e9
Júlio Cézar <jczrpereira77@gmail.com>
2025-05-24 09:48:53 -0300

    feat(components): add component config, utilities, and button with variants; update dependencies and global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${mulish.className} dark antialiased`}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={`${mulish.className} antialiased`}>{children}</body>
     </html>
   )
 }
